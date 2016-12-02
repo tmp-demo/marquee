e6cba2ef7963cd545051cd24532cba151543024a
Yorick <yorick@ctrlaltdev.xyz>
2016-12-02 22:08:06 +0100

    update css + index

--- disclaimer.js
+++ disclaimer.js
@@ -1,22 +1,26 @@
-// init allthemarquee
-allthemarquee.init(function(){
-  document.getElementById('marquee-counter').innerHTML = this.count;
-});
+var intervalDisclaimer;
 
+var disclaimer = document.querySelector('#disclaimer');
+var disclaimerMarquee = document.createElement('marquee');
+disclaimerMarquee.setAttribute("direction", "left");
+disclaimerMarquee.setAttribute("behavior", "scroll");
+disclaimerMarquee.setAttribute("truespeed","");
+disclaimerMarquee.setAttribute("scrollamount", "7");
+disclaimerMarquee.setAttribute("scrolldelay", "16");
+disclaimerMarquee.setAttribute("loop", "1");
+disclaimerMarquee.innerHTML = "Please use Firefox for this demo.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you suffer from epilepsy, please note that this demo may not be suitable for you, so turn around and refer on other people face's reactions to judge what's on the screen.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Viewer discretion is advised, every animation and motion in this demo is based on a single piece of technology, or shall we say, on a single piece of retrology.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ladies, gentlemen, and all the folks in between or apart, we present you tonight a demo only based on:";
+disclaimer.appendChild(disclaimerMarquee);
+disclaimerMarquee.addEventListener("finish",function(){
+  clearInterval(intervalDisclaimer);
+});
 // disclaimer marquee
 allthemarquee.lvlup();
 
-var disclaimer = document.querySelector('#disclaimer marquee');
-
 var accelerateDisclaimer = function(){
-  var scrollamount = parseInt(disclaimer.getAttribute('scrollamount')) + 1;
-  disclaimer.setAttribute('scrollamount', scrollamount);
+  var scrollamount = parseInt(disclaimerMarquee.getAttribute('scrollamount')) + 1;
+  disclaimerMarquee.setAttribute('scrollamount', scrollamount);
 };
 
 setTimeout(function(){
-  setInterval(accelerateDisclaimer,160*3);
+  intervalDisclaimer = setInterval(accelerateDisclaimer,160*3);
 },1000);
-
-disclaimer.addEventListener("finish",function(){
-  clearInterval(accelerateDisclaimer);
-});

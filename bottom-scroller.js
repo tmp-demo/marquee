80089dc4c268605244510215c0609f6c369967a7
Yorick <yorick@ctrlaltdev.xyz>
2016-12-03 18:54:44 +0100

    fsdmfl,

--- bottom-scroller.js
+++ bottom-scroller.js
@@ -1,10 +1,11 @@
 function bottomScroller(calback){
 
-  var scrollText = ["No animals were harmed during the making of this demo.",
+  var scrollText = [
+    "No animals were harmed during the making of this demo.",
     "Mozilla Developer Network states about <marquee> that :",
     "This feature is obsolete.",
     "Although it may still work in some browsers, [...] Try to avoid using it.",
-    "For you pleasure, coded by Gruck and Sascha, sound by Nekozilla."
+    "For you pleasure, coded by Gruck and Sascha, sound by Nekozilla.",
     "Please note that this demo may not work correctly on Netscape.",
     "Congratulations, you read the scroller this far, you won a penis enlargment kit,",
     "Do you like it so far? If you do, please imitate Chewbacca's scream right now."
